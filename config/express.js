8c6bbbb0747a0f623a90f4e11aa32b5df50c26a3
Matheus Elias <matheus__e@hotmail.com>
2015-08-12 19:40:51 -0300

    Configurado angular e rotas

--- config/express.js
+++ config/express.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var load = require('express-load');
+var bodyPaser = require('body-parser');
 
 module.exports = function(){
   var app = express();
@@ -8,6 +10,15 @@ module.exports = function(){
 
   // middleware
   app.use(express.static('./public'));
+  app.set('view engine', 'ejs');
+  app.set('views', './app/views');
+  app.use(bodyPaser.urlencoded({extended: true}));
+  app.use(bodyPaser.json());
+  app.use(require('method-override')());
+  load('models', {cwd: 'app'})
+    .then('controllers')
+    .then('routes')
+    .into(app);
 
   return app;
 };
